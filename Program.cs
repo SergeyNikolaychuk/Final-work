780f9ad428479327e74e7e6f8adf7aa052d8e0be
Your Name <you@example.com>
2023-03-12 22:53:39 +0300

    AddCondition

--- Program.cs
+++ Program.cs
@@ -2,5 +2,10 @@
 sring[] word = {"123","homer",">/"};
     for(i = 0;i< word.Length; i++)//проходим по всему массиву
 {
-    
+    for(j = 0; j<word[j].Length; j++)
+    {
+        if(word[j].Length<=3){
+            
+        }
+    }  
 }
