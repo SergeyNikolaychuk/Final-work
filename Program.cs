848f252bbdbbdcc1f0e29af4dd0e4f1f0b7ff94a
Your Name <you@example.com>
2023-03-14 21:20:06 +0300

    finish_task

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
-﻿// 
-sring[] word = {"123","homer",">/"};
-    for(i = 0;i< word.Length; i++)//проходим по всему массиву
+﻿// 1.Написать программму,которая из имеющегося массива строк формирует массив из строк,длина которых
+// меньше либо равна 3 символам.
+string[] word = { "123", "homer", ">/" };
+List<string> filteredList = new List<string>();
+for (int i = 0; i < word.Length; i++)//проходим по всему массиву
 {
-    for(j = 0; j<word[j].Length; j++)
+    if (word[i].Length <= 3)
     {
-        if(word[j].Length<=3){
-            
-        }
-    }  
+        filteredList.Add(word[i]);
+    }
+
 }
+    string[] result = filteredList.ToArray();
+    foreach (string a in result)
+    {
+        Console.WriteLine(a);
+    }
+
